c809c5f09f119f13740802b9089015790a3e87b4
soy206 <21500035@handong.edu>
2019-12-30 16:05:56 +0900

    after axios

--- pages/doro.vue
+++ pages/doro.vue
@@ -1,25 +1,30 @@
 <template>
-    <v-app>
-        <v-layout class="gcolor">
-        <v-app-bar
-            app="app"
-            color="#2D9527"
-            flat="flat">
-        </v-app-bar>
-        </v-layout>
-    </v-app>
-</template>
+    <v-layout column="column" row="row">
+        <v-flex wrap="wrap"> 
+            <v-list-item>
+                <v-list-item-content>
+                    <v-text-field class="caption" placeholder="지번, 건물명, 도로명 입력"></v-text-field>
+                </v-list-item-content>
+            </v-list-item>
 
+            <div class="being">
+                <v-btn max-width="80%" min-width="80%" color="#2D9527" dark="dark">검색하기</v-btn>
+            </div>
+
+        </v-flex>
+    </v-layout>
+
+</template>
 <script>
-    export default {
-        data() {
-            return {}
-        }
-    }
+    export default {name: 'chwideug'}
 </script>
 
-<style>
-    .gcolor {
-        background: #2D9527;
+<style scoped="scoped">
+    .btn {
+        border: rgb(220, 226, 233);
+    }
+    .being {
+        display: flex;
+        justify-content: center;
     }
 </style>
